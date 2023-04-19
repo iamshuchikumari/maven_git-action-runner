158bf59dd3b304540718af7cce5c3d0f6330751a
iamshuchikumari <92261673+iamshuchikumari@users.noreply.github.com>
2023-04-19 09:17:25 +0530

    Add files via upload
    created maven project

---
+++ src/main/java/com/shuchi/App.java
@@ -0,0 +1,13 @@
+package com.shuchi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! \n This Code is executed by Shuchi on Git Acrion Runner using Maven Build tool" );
+    }
+}
